574ec77646574ed02ac30c8947a10bf3eb32964b
Pete Low <pedritolow@gmail.com>
2021-11-04 16:53:59 -0700

    added contactscreen to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import ContactScreen from './screens/ContactScreen';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
@@ -12,6 +13,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/contact' component={ContactScreen} exact />
         </Container>
       </main>
       <Footer />
