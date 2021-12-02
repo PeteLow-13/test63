6297695057368bedc38c772135d42f44eea983c0
Pete Low <pedritolow@gmail.com>
2021-12-02 15:14:00 -0800

    Created Destination Screen

---
+++ frontend/src/screens/DestinantionScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DestinantionScreen = () => {
+  return (
+    <div>
+      <h1>Destinantion Screen</h1>
+    </div>
+  );
+};
+
+export default DestinantionScreen;
