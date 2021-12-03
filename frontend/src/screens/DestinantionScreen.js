9e325e84e73bd47200cab79cf25552ec4f172b05
Pete Low <pedritolow@gmail.com>
2021-12-02 18:20:38 -0800

    added cards to location screen

--- frontend/src/screens/DestinantionScreen.js
+++ frontend/src/screens/DestinantionScreen.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import PoulsboCard from '../components/PoulsboCard';
+import PointNoPointCard from '../components/PointNoPointCard';
+import PortGambleCard from '../components/PortGambleCard';
+import KingstonCard from '../components/KingstonCard';
 
 const DestinantionScreen = () => {
   return (
-    <div>
-      <h1>Destinantion Screen</h1>
-    </div>
+    <>
+      <Link to='/' className='btn btn-light'>
+        Home
+      </Link>
+      <h1>Destinantions</h1>
+      <Row>
+        <Col sm={12} md={12} lg={6} xl={6}>
+          <PoulsboCard />
+        </Col>
+        <Col sm={12} md={12} lg={6} xl={6}>
+          <PointNoPointCard />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} md={12} lg={6} xl={6}>
+          <KingstonCard />
+        </Col>
+        <Col sm={12} md={12} lg={6} xl={6}>
+          <PortGambleCard />
+        </Col>
+      </Row>
+    </>
   );
 };
 
